ba5a4b6ca746168d3282222ae684d5824503c7f2
MrEuclid <john@teacherjohn.org>
2024-08-24 14:02:14 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+datalogger.mirrorToSerial(true)
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
 basic.forever(function () {
     basic.showNumber(input.temperature())
+    datalogger.log(datalogger.createCV("Temperature", input.temperature()))
 })
